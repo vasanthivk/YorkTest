c299697868faa95d87107cbab01bd7e7c09bea4e
JUNIOR12\master <sportydevelopment@gmail.com>
2017-09-27 11:59:39 +0530

    no message

---
+++ Source/apps/Customer/FoodAdvisr/www/js/map.js
@@ -0,0 +1,10 @@
+
+function myMap() {
+	alert("From map");
+var mapOptions = {
+    center: new google.maps.LatLng(51.5, -0.12),
+    zoom: 10,
+    mapTypeId: google.maps.MapTypeId.HYBRID
+}
+var map = new google.maps.Map(document.getElementById("map"), mapOptions);
+}
