da319d4f10460852f799c543d527a0a95e88d33c
bandaruvenu <bandaruvenu@gmail.com>
2017-10-12 18:40:34 +0530

    no message

--- Source/api/FoodAdvisr/app/Api_V1_GetAllDetails.php
+++ Source/api/FoodAdvisr/app/Api_V1_GetAllDetails.php
@@ -52,7 +52,7 @@ ini_set('max_execution_time', 5000);
 
 	function v1_gettop10eateries($latitude,$longitude)
 	{
-		$sql  = 'select *,round((6371*0.621371 * 2 * ASIN(SQRT( POWER(SIN(("'.$latitude.'" - abs(latitude)) * pi()/180 / 2),2) +  COS("'.$latitude.'" * pi()/180 ) * COS(abs(latitude) * pi()/180) * POWER(SIN(("'.$longitude.'" - longitude) * pi()/180 / 2), 2) ))),2) as distance from eateries LIMIT 10';
+		$sql  = 'select *,round((6371*0.621371 * 2 * ASIN(SQRT( POWER(SIN(("'.$latitude.'" - abs(latitude)) * pi()/180 / 2),2) +  COS("'.$latitude.'" * pi()/180 ) * COS(abs(latitude) * pi()/180) * POWER(SIN(("'.$longitude.'" - longitude) * pi()/180 / 2), 2) ))),2) as distance from eateries  ORDER BY distance, IFNULL(IsAssociated,0) DESC LIMIT 10';
 		$result = DB::select( DB::raw($sql));
 		return $result;
 	}
