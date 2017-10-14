f200844b5df888a34aa61bb518c8af72189f085a
JUNIOR12\master <sportydevelopment@gmail.com>
2017-10-14 18:37:13 +0530

    no message

--- Source/apps/Customer/FoodAdvisr7Oct/www/js/map.js
+++ Source/apps/Customer/FoodAdvisr7Oct/www/js/map.js
@@ -14,17 +14,49 @@ var marker = new google.maps.Marker({
       title: 'you are here!'
     });
 
-    var marker;
+    var marker,idx;
   api.getEateries(latitude, longitude, searchval,function(data){
         //var op ='';
         if(data.result.length > 0)
         {
-            for (idx in data.result) {  
+            for (idx = 0; idx < data.result.length; idx++) {  
                // alert(data.result[idx].Latitude);
           marker = new google.maps.Marker({
             position: new google.maps.LatLng(data.result[idx].Latitude, data.result[idx].Longitude),
             map: map
           });
+          google.maps.event.addListener(marker, 'click', (function(marker, idx) {
+            return function() {
+          if(data.result[idx].IsAssociated == 1)
+            {
+          var infowindow = new google.maps.InfoWindow({
+             content: '<div class="act-eatery">' +
+                      '<input type=hidden id="eateryId" value="' + data.result[idx].id + '" />' + 
+                      '<div>' +
+                        '<div class="act-eatery-logo" ><img class="act-eatery-logopath" src="' + appSettings.mediaPath + data.result[idx].LogoPath + '"></img></div>' +
+                        '<div class="act-eatery-name"><b>'+ data.result[idx].BusinessName + '</b><br/>'+ '<div class="act-action-div"><div class="act-eatery-distance">'+ data.result[idx].distance+'m'+ '&nbsp&nbsp&nbsp|'+'</div>'+'<div class="act-eatery-image"> <img class="act-eatery-image" src="img/foodadvisr-green.png"/></div></div>' +'</div>' +
+                      '</div>' +
+                      '<div class="eatery-clear"></div>' +
+                    '</div>'
+          });
+          infowindow.open(map, marker);
+        }
+        else{
+           var infowindow = new google.maps.InfoWindow({
+             content: '<div class="in-act-eatery">' +
+                      '<input type=hidden id="eateryId" value="' + data.result[idx].id + '" />' + 
+                      '<input type=hidden id="eateryName" value="' + data.result[idx].BusinessName + '" />' + 
+                      '<div>' +
+                        '<div class="in-act-eatery-logo" ><img class="act-eatery-logopath" src="../img/thumb.svg"></img></div>' + 
+                        '<div class="in-act-eatery-name"><b>'+ data.result[idx].BusinessName + '</b><br/>' +/* (data.result[idx].Address==null?'':data.result[idx].Address)+*/'<div class="act-action-div"><div class="act-eatery-distance">'+ data.result[idx].distance+'m'+'&nbsp&nbsp&nbsp|'+'</div>'+'<div class="act-eatery-image"> <img class="in-act-eatery-image" src="img/foodadvisr-green.png"/></div></div>' +'</div>' + '</div>' +
+                      '</div>' +
+                      '<div class="eatery-clear"></div>' +
+                    '</div>'
+          });
+          infowindow.open(map, marker);
+        }
+            }
+      })(marker, idx));
         } 
     }
  });
