74314211cf12e60ab32e6d11584d1bcfc60cdaa1
DESKTOP-QVSA6M9\Raviteja Ch <ch.raviteja2@gmail.com>
2017-10-14 12:15:18 +0530

    no message

--- Source/api/FoodAdvisr/app/Api_V1_GetAllDetails.php
+++ Source/api/FoodAdvisr/app/Api_V1_GetAllDetails.php
@@ -12,28 +12,39 @@ ini_set('max_execution_time', 5000);
     	 $sql  = "select id
             from eateries where (6371*0.621371 * 2 * ASIN(SQRT( POWER(SIN(($latitude - abs(latitude)) * pi()/180 / 2),2) +  COS($latitude * pi()/180 ) * COS(abs(latitude) * pi()/180) * POWER(SIN(($longitude - longitude) * pi()/180 / 2), 2) ))  <= $search_radius) ";
         $result = DB::select( DB::raw($sql));
-        $hotelsids = [];
+        $eateriesids = [];
          foreach($result as $result1)
     	{
-        	$hotelsids[] = $result1->id; 
+        	$eateriesids[] = $result1->id; 
     	}
     	
-    	$hotels = DB::table('eateries')
+    	$eateries = DB::table('eateries')
     		    ->select(DB::raw('id, FHRSID, BusinessName, Address, LogoPath, IsAssociated, Longitude, Latitude, FoodAdvisrOverallRating, round((6371*0.621371 * 2 * ASIN(SQRT( POWER(SIN(("'.$latitude.'" - abs(latitude)) * pi()/180 / 2),2) +  COS("'.$latitude.'" * pi()/180 ) * COS(abs(latitude) * pi()/180) * POWER(SIN(("'.$longitude.'" - longitude) * pi()/180 / 2), 2) ))),2) as distance'))
-    		   ->wherein('FHRSID',$hotelsids)
+    		   ->wherein('id',$eateriesids)
                ->orderby('distance','asc')
                ->limit($search_result_limit)
     		   ->get();
 
-    	return $hotels;
+    	return $eateries;
 	}
 
     function v1_geteaterydetailsbyid($id)
     {
         $result = Eateries::find($id);
         if($result <> null)
+            $result->distance = getDistanceById($id);
             $result->media = getImagesById($id);
         return $result;     
+    }
+
+    function getDistanceById($id)
+    {
+       $result = Eateries::find($id);
+       $get_distance = DB::table('eateries')
+                ->select(DB::raw('round((6371*0.621371 * 2 * ASIN(SQRT( POWER(SIN(("'.$result->Latitude.'" - abs(latitude)) * pi()/180 / 2),2) +  COS("'.$result->Latitude.'" * pi()/180 ) * COS(abs(latitude) * pi()/180) * POWER(SIN(("'.$result->Longitude.'" - longitude) * pi()/180 / 2), 2) ))),2) as distance'))
+               ->where('id','=',$id)
+               ->get();
+       return $get_distance[0]->distance; 
     }
 
     function getImagesById($id)
