8bf146723ac5a2789ab06d89e80204405e57aff0
DESKTOP-QVSA6M9\Raviteja Ch <ch.raviteja2@gmail.com>
2017-10-25 10:01:03 +0530

    no message

--- Source/api/FoodAdvisr/resources/views/items/create.blade.php
+++ Source/api/FoodAdvisr/resources/views/items/create.blade.php
@@ -286,7 +286,6 @@ Items
         </div>
 </div>
 
-@endsection
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
 <script>
     $(document).ready(function(){
