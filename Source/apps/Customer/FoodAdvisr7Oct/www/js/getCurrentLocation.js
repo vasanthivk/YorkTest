a252e2e2aef7b683ec4cfcb2f85d14c3152a3775
JUNIOR12\master <sportydevelopment@gmail.com>
2017-10-26 17:19:08 +0530

    no message

---
+++ Source/apps/Customer/FoodAdvisr7Oct/www/js/getCurrentLocation.js
@@ -0,0 +1,36 @@
+function GetCurrentLocation(){
+ 
+  navigator.geolocation.getCurrentPosition(onSuccess, onError,{enableHighAccuracy: true});
+
+}
+function onSuccess(position) {
+     // alert("position.coords.latitude" + position.coords.latitude);
+       var myLatlng = new google.maps.LatLng(position.coords.latitude,position.coords.longitude);
+         var geocoder = new google.maps.Geocoder;
+           geocoder.geocode({'location': myLatlng}, function(results, status) {
+              if (status === google.maps.GeocoderStatus.OK) {
+                if (results[1]) {
+                  //alert(results[1].formatted_address);
+                 document.getElementById("eatery-search").value =  results[1].formatted_address; 
+                  eaterySearchReset();
+                }
+            }
+        });
+      
+    }
+
+    // onError Callback receives a PositionError object
+    //
+    function onError(error) {
+        alert('code: '    + error.code    + '\n' +
+              'message: ' + error.message + '\n');
+    }
+
+
+//Event for device ready
+ document.addEventListener("deviceready", onDeviceReady, false);
+
+function onDeviceReady() {
+    // Now safe to use the PhoneGap API
+    GetCurrentLocation();
+}
