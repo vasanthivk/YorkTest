db7447f52a1858826cc87df9b4bc1e640c62646a
JUNIOR12\master <sportydevelopment@gmail.com>
2017-10-26 17:48:04 +0530

    no message

--- Source/apps/Customer/FoodAdvisr7Oct/www/js/loadingOverlay.js
+++ Source/apps/Customer/FoodAdvisr7Oct/www/js/loadingOverlay.js
@@ -1,8 +1,10 @@
 
 function openLoading() {
     document.getElementById("myNav").style.width = "100%";
+    alert(document.getElementById("myNav").style.width);
 }
 
 function closeLoading() {
     document.getElementById("myNav").style.width = "0%";
+    alert(document.getElementById("myNav").style.width);
 }
