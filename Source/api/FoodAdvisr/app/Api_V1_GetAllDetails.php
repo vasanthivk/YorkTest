7653ded7f6fe9e5946233157caf6159c9d6cf3d4
DESKTOP-QVSA6M9\Raviteja Ch <ch.raviteja2@gmail.com>
2017-10-27 15:19:12 +0530

    no message

--- Source/api/FoodAdvisr/app/Api_V1_GetAllDetails.php
+++ Source/api/FoodAdvisr/app/Api_V1_GetAllDetails.php
@@ -4,7 +4,7 @@ use App\Eateries;
 ini_set('memory_limit', '5048M');
 ini_set('max_execution_time', 5000);
 
-	function v1_geteateries($latitude,$longitude)
+	function v1_geteateries($latitude,$longitude,$cuisines_ids,$lifestyle_choices_ids)
 	{
 		 $defaults = Defaults::all();    
          $search_radius = $defaults[0]->search_radius;
@@ -13,7 +13,7 @@ ini_set('max_execution_time', 5000);
                     round((6371*0.621371 * 2 * ASIN(SQRT( POWER(SIN(($latitude - abs(latitude)) * pi()/180 / 2),2) 
                     +  COS($latitude * pi()/180 ) * COS(abs(latitude) * pi()/180) * POWER(SIN(($longitude - longitude) 
                     * pi()/180 / 2), 2) ))),2) as distance
-                    from eateries where id  in (select id from eateries where (6371*0.621371 * 2 * ASIN(SQRT( POWER(SIN(($latitude - abs(latitude)) * pi()/180 / 2),2) +  COS($latitude * pi()/180 ) * COS(abs(latitude) * pi()/180) * POWER(SIN(($longitude - longitude) * pi()/180 / 2), 2) ))  <= $search_radius) )  order by IsAssociated desc, distance asc
+                    from eateries where id  in (select id from eateries where (6371*0.621371 * 2 * ASIN(SQRT( POWER(SIN(($latitude - abs(latitude)) * pi()/180 / 2),2) +  COS($latitude * pi()/180 ) * COS(abs(latitude) * pi()/180) * POWER(SIN(($longitude - longitude) * pi()/180 / 2), 2) ))  <= $search_radius) ) and  cuisines_ids in($cuisines_ids) and lifestyle_choices_ids in($lifestyle_choices_ids) order by IsAssociated desc, distance asc
                     limit $search_result_limit";
          $eateries = DB::select( DB::raw($sql));
               
