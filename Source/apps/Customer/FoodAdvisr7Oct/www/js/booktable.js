05afc047ac76a406a569be2e88c6c722d9f74526
= <naresh.budda@hellocurry.com>
2017-10-28 19:01:06 +0530

    modified buttons

--- Source/apps/Customer/FoodAdvisr7Oct/www/js/booktable.js
+++ Source/apps/Customer/FoodAdvisr7Oct/www/js/booktable.js
@@ -100,7 +100,7 @@
       $('body').on('click','.popupbook-act-button',function(){
         // overlay.hide();
       var message ="This will be live soon. We'll let you know when it's available!";
-      popupcall.show(message,'  Ok  ,Cancel');
+      popupcall.show(message,'  Cancel,Ok  ');
         // return false;
       });
 
