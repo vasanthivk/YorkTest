93a1071dfe81972b231ad494e65f0bd3be50834d
DESKTOP-QVSA6M9\Raviteja Ch <ch.raviteja2@gmail.com>
2017-10-30 15:49:04 +0530

    no message

--- Source/api/FoodAdvisr/config/app.php
+++ Source/api/FoodAdvisr/config/app.php
@@ -163,6 +163,8 @@ return [
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
         Collective\Html\HtmlServiceProvider::class,
+
+         Maatwebsite\Excel\ExcelServiceProvider::class,
         /*
          * Package Service Providers...
          */
@@ -228,6 +230,7 @@ return [
         'Input' => Illuminate\Support\Facades\Input::class,
         'Form' => Collective\Html\FormFacade::class,
         'Html' => Collective\Html\HtmlFacade::class,
+        'Excel' => Maatwebsite\Excel\Facades\Excel::class,
     ],
 
 ];
