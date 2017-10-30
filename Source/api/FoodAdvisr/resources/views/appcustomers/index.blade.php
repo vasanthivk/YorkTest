313baae3a7a2e9be6fa117c3846cc7559e44bc94
DESKTOP-QVSA6M9\Raviteja Ch <ch.raviteja2@gmail.com>
2017-10-30 16:10:02 +0530

    no message

--- Source/api/FoodAdvisr/resources/views/appcustomers/index.blade.php
+++ Source/api/FoodAdvisr/resources/views/appcustomers/index.blade.php
@@ -10,19 +10,12 @@ Customers
 @include('components.message')	
 <div class="row">
     <div class="col-md-12">
-		<div class="panel panel-default">
-                                <!-- <div class="panel-heading">          
-                                    <div class="btn-group pull-left">
-                                    @if($privileges['Add']=='true') 
-                                        <a href="{{URL::to('appcustomers/create')}}" class="btn btn-info"><i class="fa fa-edit"></i>Add Customers</a>
-                                        @endif
-                                    </div>
-                                </div> -->
+		<div class="panel panel-default">                               
                                 <div class="panel-body">
                                     <table id="customers2" class="table datatable">
                                         <thead>
                                             <tr>
-                                                <th>Full Name</th>
+                                                <th></th>
                                                 <th>Email</th>
                                                 <th>Status</th>
                                             </tr>
@@ -30,7 +23,7 @@ Customers
                                         <tbody>                                                    @foreach($appcustomers as $customer)
                                     <tr>
                                         <td>
-                                            {{$customer->fullname}}
+                                            
                                         </td>       
                                         <td>{{$customer->email}}</td>
                                         <td>{{$customer->status}}</td>          
