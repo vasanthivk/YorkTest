ae8d12efa934daa5a4b35b7cd169885489df4a23
DESKTOP-N7UALF9\Saikumar <saikumar@acharis.com>
2017-11-02 18:48:59 +0530

    modified

--- Source/api/FoodAdvisr/app/Api_V1_GetAllDetails.php
+++ Source/api/FoodAdvisr/app/Api_V1_GetAllDetails.php
@@ -62,7 +62,7 @@ ini_set('max_execution_time', 5000);
             $result->distance = getDistanceById($id);
             $result->media = getImagesById($id);
             $result->menus = getmenubygroupid($result->group_id,$id);
-            $result->dishes = geteaterydishes($result->group_id,$id);
+            $result->dishes = geteaterydishes($id);
         }
         return $result;     
     }
@@ -270,7 +270,7 @@ ini_set('max_execution_time', 5000);
 
        return $menuDetails;
     }
-    function geteaterydishes($groupid,$eateryid)
+    function geteaterydishes($eateryid)
     {
         $dish_details = DB::table('dishes')
             ->where('dishes.eatery_id', '=', $eateryid)
